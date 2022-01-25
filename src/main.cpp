c006d6360281a8e63ae007ff8bff08ae625657f5
artemka <63093588+Artem4ik05@users.noreply.github.com>
2022-01-25 20:35:22 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,39 @@
 #define adress 5
 #define bit 11
 
-#define P
-#define I
-#define D
+float P = 1.0;
+float I = 1.0;
+float D = 1.0;
+long long error;
+long long p_error;
+long long guess;
+long long Min;
+long long Max;
+int speed;
 
 byte mas1[11], mas[9];
 
-int PID(int value)
+int PID(int value, int speed)
 {
+  long long delta = value - guess;
+  // P part
+  long long p = delta * P;
 
+  // D part
+  error = delta;
+
+  long long d = (error - p_error) * D;
+
+  p_error = error;
+  // I part
+  long long i; i += error;
+  if (i < Min) i = Min;
+  if (i > Max) i = Max;
+
+  long long res = p + i + d;
+  if (res > Max) return Max;
+  if (res < Min) return Min;
+  return res; 
 }
 
 int values_color()
@@ -32,7 +56,7 @@ int values_color()
   for (int i = 0; i < 8; ++i)
     mas[i] = mas1[i];
   
-  mas[8] = (mas1[8] + mas1[9] + mas2[10]) / 3;
+  mas[8] = (mas1[8] + mas1[9] + mas1[10]) / 3;
 
   long long sum = 0;
   sum = (mas[0] * 1 + mas[1] * 256 + mas[2] * 512 + mas[3] * 768 + mas[4] * 1024 + mas[5] * 1280 + mas[6] * 1536 + mas[7] * 1792 + mas[8] * 2048) / (mas[0] + mas[1] + mas[2] + mas[3] + mas[4] + mas[5] + mas[6] + mas[7] + mas[8]);
@@ -57,5 +81,4 @@ void setup()
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
 }
